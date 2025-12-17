b5f83ca687b00362358cc165c6af6d3d63366682
unknown <shwetakumari3405@gmail.com>
2025-12-18 00:40:28 +0530

    [shweta]:added CalculateSimpleInterest file

--- gcr-codebase/Program-Prerequisite/CalculateSimpleInterest.java
+++ gcr-codebase/Program-Prerequisite/CalculateSimpleInterest.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+//creating a class with name CalculateSimpleInterest 
 public class CalculateSimpleInterest{
 public static void main(String[]args){
 Scanner input=new Scanner(System.in);
 
+//Declaring the variable and reading input from the user 
 double Principal, Rate, Time, Result;
 System.out.println("Enter the Principal Amount");
 Principal=input.nextDouble();
@@ -10,7 +12,12 @@ System.out.println("Enter the Rate");
 Rate=input.nextDouble();
 System.out.println("Enter the Time");
 Time=input.nextDouble();
+
+//calculating the Simple Interest value
 Result=((Principal*Rate*Time)/100);
+
+//Displaying the result
 System.out.println("The resultant Simple Interest is "+Result);
+input.close();
 }
 }
