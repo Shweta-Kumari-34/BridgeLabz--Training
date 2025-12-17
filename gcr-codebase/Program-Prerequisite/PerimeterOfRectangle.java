78d75c893a6d466181c4030efa15886127c81ccc
unknown <shwetakumari3405@gmail.com>
2025-12-18 00:55:14 +0530

    [shweta]:added PerimeterOfRectangle file

--- gcr-codebase/Program-Prerequisite/PerimeterOfRectangle.java
+++ gcr-codebase/Program-Prerequisite/PerimeterOfRectangle.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
+//creating a class with name as PerimeterOfRectangle
 public class PerimeterOfRectangle{
 public static void main(String[]args){
 Scanner input=new Scanner(System.in);
-Double length,width,Perimeter;
+
+// Declaring the variable and reading the value from user
+double length,width,perimeter;
 System.out.println("Enter The Value of Length");
 length=input.nextDouble();
 System.out.println("Enter The Value of Width");
 width=input.nextDouble();
-Perimeter=2*(length+width);
-System.out.println("The resultant Perimeter of the Rectangle is "+Perimeter);
 
+//calculating the perimeter of the rectangle
+perimeter=2*(length+width);
+
+//Displaying the result
+System.out.println("The resultant Perimeter of the Rectangle is "+perimeter);
+input.close();
 }
 }
