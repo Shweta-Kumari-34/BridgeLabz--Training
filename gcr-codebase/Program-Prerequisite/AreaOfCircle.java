7611fd3af1ffb723d656f3815012e398e3df650e
unknown <shwetakumari3405@gmail.com>
2025-12-18 00:26:49 +0530

    [shweta]:added AreaOfCircle file

--- gcr-codebase/Program-Prerequisite/AreaOfCircle.java
+++ gcr-codebase/Program-Prerequisite/AreaOfCircle.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
+//creating a class with name AreaOfCircle to calculate the Area of Circle
 public class AreaOfCircle{
 public static void main(String[]args){
 Scanner input=new Scanner(System.in);
+
+//  Declaring variables and reading  value from user
 double radius, Area;
 double pi=3.14;
 System.out.println("Enter the radius of Circle");
 radius=input.nextDouble();
+
+//calculating the area of circle and storing it into Area variable
 Area= pi* radius*radius;
+
+//Displaying the result
 System.out.println("The Resultant Area of Circle is "+Area);
+input.close();
 }
 }
