6ac0be71b9d9c7425bb912430f4ed70f1967d633
unknown <shwetakumari3405@gmail.com>
2025-12-17 15:11:29 +0530

    [shweta]:added PrintWelcomeToBridgelabz file

--- gcr-codebase/Program-Prerequisite/PrintWelcomeToBridgelabz.java
+++ gcr-codebase/Program-Prerequisite/PrintWelcomeToBridgelabz.java
@@ -1,3 +1,4 @@
+// creating a class named as PrintWelcomeToBridgelabz to print Welcome to Bridgelabz
 public class PrintWelcomeToBridgelabz{
 public static void main(String[]args){
 System.out.println("Welcome to Bridgelabz");
