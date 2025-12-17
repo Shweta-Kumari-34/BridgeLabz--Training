0d253ba0746a2ac6c65de6afbd6cfa362132d186
unknown <shwetakumari3405@gmail.com>
2025-12-18 00:37:13 +0530

    [shweta]:added VolumeOfCylinder file

--- gcr-codebase/Program-Prerequisite/VolumeOfCylinder.java
+++ gcr-codebase/Program-Prerequisite/VolumeOfCylinder.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+//creating a class with name as VolumeOfCylinder
 public class VolumeOfCylinder{
 public static void main(String[]args){
 Scanner input=new Scanner(System.in);
-double radius, height, Volume, pi;
-pi=3.14;
+
+//Declaring the variables and reading the value from the user
+double radius, height, volume;
 System.out.println("Enter the radius of the cylinder");
 radius= input.nextDouble();
 System.out.println("Enter the height of the cylinder");
 height=input.nextDouble();
-Volume=pi*radius*radius*height;
-System.out.println(" The Resultant Volume of The Cylinder is " +Volume);
+
+//calculating the volume of the cylinder
+volume=Math.PI*radius*radius*height;
+
+//Displaying the result
+System.out.println(" The Resultant Volume of The Cylinder is " +volume);
+input.close();
 }
 }
 
