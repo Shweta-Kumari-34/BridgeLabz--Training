cd642e93a63cfd954ac124b3060749e52cb4ddae
unknown <shwetakumari3405@gmail.com>
2025-12-17 15:31:33 +0530

    [shweta]: added AddTwoNumbers file

--- gcr-codebase/Program-Prerequisite/AddTwoNumbers.java
+++ gcr-codebase/Program-Prerequisite/AddTwoNumbers.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+/*creating a class named as AddTwoNumbers inside it creating a method named as addTwoNumbers  that take two Parameter named as number1 and number2 it perform the sum operation and store the result in sum variable and return the sum */ 
 class AddTwoNumbers{
-static int addTwoNumbers(int x, int y){
-int sum=x+y;
+static int addTwoNumbers(int number1, int number2){
+int sum=number1+number2;
 return sum;
 }
 public static void main(String[]args){
 Scanner input=new Scanner(System.in);
-int num1,num2, sum, result;
+int number1,number2, sum, result;
 System.out.println("Enter first number");
-num1=input.nextInt();
+number1=input.nextInt();
 System.out.println("Enter Second number");
-num2=input.nextInt();
-result= addTwoNumbers(num1,num2);
+number2=input.nextInt();
+// Here we are calling the method named as addTwoNumbers and passing the arguments and storing the result in result variable
+result= addTwoNumbers(number1,number2);
 System.out.println("Addition of two numbers is ="+result);
 }
 }
