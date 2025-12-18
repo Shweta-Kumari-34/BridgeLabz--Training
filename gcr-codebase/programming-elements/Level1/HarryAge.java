2206bd81be57c31bee86d6a6529787c82f55e800
unknown <shwetakumari3405@gmail.com>
2025-12-18 14:18:19 +0530

    [shweta]:added HarryAge file

---
+++ gcr-codebase/programming-elements/Level1/HarryAge.java
@@ -0,0 +1,13 @@
+//creating class with name as HarryAge
+public class HarryAge{
+public static void main(String[]args){
+int birthYear=2000;
+int currentYear=2024;
+
+//Calculating the result
+int result=currentYear-birthYear;
+
+//Displaying the result
+System.out.println("Harry's age in 2024 is "+result);
+}
+}
