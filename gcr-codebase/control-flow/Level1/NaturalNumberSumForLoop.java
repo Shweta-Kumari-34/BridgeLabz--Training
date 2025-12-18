36e01493cff3233e6dba50859ee8ef4f8d5c969d
unknown <shwetakumari3405@gmail.com>
2025-12-18 21:52:17 +0530

    [shweta]: added NaturalNumberSumForLoop file

---
+++ gcr-codebase/control-flow/Level1/NaturalNumberSumForLoop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+//Creating a class with name as NaturalNumberSum
+//to calculate the sum of n natural number using formula n*(n+1)/2 as well as for loop
+public class NaturalNumberSumForLoop{
+public static void main(String[]args){
+Scanner input=new Scanner(System.in);
+
+//Declaring the Variables and reading input from user
+int number;
+double sum1=0.0;
+double sum2=0.0;
+System.out.println("Enter the number ");
+number=input.nextInt();
+int n=number;
+
+//first check the given number is natural or not
+//then calculating the sum of n natural number using both ways
+//comparing the sum1 and sum2 result and displaying the result
+if(number>=0){
+sum1=number*(number+1)/2;
+for( int count= number; count >=0; count--){
+sum2=sum2+count;
+}
+}
+if(sum1==sum2){
+System.out.println("The sum of "+n+" natural number is "+sum1);
+}
+input.close();
+}
+}
+
+
