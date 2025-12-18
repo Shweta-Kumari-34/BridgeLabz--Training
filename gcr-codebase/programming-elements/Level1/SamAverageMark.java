bc57414bc8fbaafea5307a45558d0e9270603864
unknown <shwetakumari3405@gmail.com>
2025-12-18 21:22:44 +0530

    [shweta]: added SamAverageMark file

---
+++ gcr-codebase/programming-elements/Level1/SamAverageMark.java
@@ -0,0 +1,14 @@
+//creating a class with name as SamAverageMark
+public class SamAverageMark{
+public static void main(String[]args){
+int mathMark=94;
+int physicsMark=95;
+int chemistryMark=96;
+
+//calculating average mark
+int result=((mathMark+physicsMark+chemistryMark)/3);
+
+//Displaying the result
+System.out.println("Sam's average mark in PCM is "+result);
+}
+}
