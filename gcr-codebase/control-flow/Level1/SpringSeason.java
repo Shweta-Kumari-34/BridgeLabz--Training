f92e4f143edf4f303f78d00b576c4bb05b91d9a8
unknown <shwetakumari3405@gmail.com>
2025-12-19 10:54:38 +0530

    [shweta]: added SpringSeason file

---
+++ gcr-codebase/control-flow/Level1/SpringSeason.java
@@ -0,0 +1,18 @@
+//Creating a class with name as SpringSeason
+public class SpringSeason{
+public static void main(String[]args){
+
+//reading input from command line 
+int month =Integer.parseInt(args[0]);
+int day=Integer.parseInt(args[1]);
+
+//checking conditions and displaying result
+boolean isSpring=(month==3 && day>=20) ||(month ==4) ||(month ==5)||(month ==6 && day<=20);
+if(isSpring){
+System.out.println("Its a Spring Season ");
+}
+else {
+System.out.println("Not a Spring Season ");
+}
+}
+}
