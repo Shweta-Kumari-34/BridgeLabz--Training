32eef7f88ae09f66c286e39b8b3eeb6b6c43894d
unknown <shwetakumari3405@gmail.com>
2025-12-20 07:43:52 +0530

    [shweta]: added Multiplication Table  file

---
+++ gcr-codebase/arrays/Level1/MultiplicationTable.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//Creating a class with name as MultiplicationTable
+public class MultiplicationTable{
+public static void main(String[]args){
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter  a number ");
+int  number =input.nextInt();
+int  result[] =new int [4] ;
+int index =0;
+for( int i=6; i <=9; i++){
+result[index]=number * i ;
+index++;
+}
+int index =0;
+for( int i=6; i <=9; i++){
+System.out.println(number + " * i " +i + " = " + result[index] );
+index++;
+}
+input.close();
+}
+}
