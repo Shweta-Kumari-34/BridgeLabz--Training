b2468a442ad25db87240d5a3106fe3b4847c0f45
unknown <shwetakumari3405@gmail.com>
2025-12-23 22:07:41 +0530

    [shweta]: Number Check program

---
+++ gcr-codebase/methods/Level3/NumberCheck.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+// Creating class with name as NumberCheck
+public class NumberCheck {
+
+// Method to check whether number is positive or not
+public static boolean isPositive(int number) {
+if (number >= 0) {
+return true;
+} else {
+return false;
+}
+}
+
+//Method to check whether number is even
+public static boolean isEven(int number) {
+if (number % 2 == 0) {
+return true;
+} else {
+return false;
+}
+}
+
+// Method to compare two numbers
+public static int compare(int number1, int number2) {
+if (number1 > number2) {
+return 1;                //if number1 > number2
+} else if (number1 == number2) {
+return 0;                //if number1 == number2
+} else {
+return -1;               //if number1 < number2
+}
+}
+
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+// Array to store 5 numbers
+int[] numbers = new int[5];
+
+// reading user input
+for (int i = 0; i < numbers.length; i++) {
+System.out.print("Enter number " + (i + 1));
+numbers[i] = input.nextInt();
+}
+
+//Checking  conditions for (positive/negative) and (even/odd)
+for (int i = 0; i < numbers.length; i++) {
+if ( isPositive(numbers[i])) {
+System.out.println(numbers[i] + " is Positive and "); 
+if (isEven(numbers[i])) {
+System.out.println("Even");
+} else {
+System.out.println("Odd");
+}
+} else {
+System.out.println(numbers[i] + " is Negative");
+}
+}
+// Comparing first and last element
+int result = compare(numbers[0], numbers[numbers.length - 1]);
+if(result ==1 ){
+System.out.println("First element is Greater than last element");
+} else  if (result == 0) {
+System.out.println("First and last elements are Equal");
+} else {
+System.out.println("First element is Less than last element");
+}
+
+input.close();
+}
+}
