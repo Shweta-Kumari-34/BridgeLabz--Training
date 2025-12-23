a5ec07be40a736ba1248c1daaa40ca7b37bf5d29
unknown <shwetakumari3405@gmail.com>
2025-12-23 16:08:15 +0530

    [shweta]: Unit convertor1 program

---
+++ gcr-codebase/methods/Level2/UnitConvertor.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+//Creating a utility class with name as UnitConvertor2
+public class UnitConvertor {
+
+// Method to convert yards to feet
+public static double convertYardsToFeet(double yards) {
+double yards2feet = 3;
+return yards * yards2feet;
+}
+
+//Method to convert feet to yards
+public static double convertFeetToYards(double feet) {
+double feet2yards = 0.333333;
+return feet * feet2yards;
+}
+//Method to convert meters to inches
+public static double convertMetersToInches(double meters) {
+double meters2inches = 39.3701;
+return meters * meters2inches;
+}
+
+//Method to convert inches to meters
+public static double convertInchesToMeters(double inches) {
+double inches2meters = 0.0254;
+}
+
+//Method to convert inches to centimeters
+public static double convertInchesToCentimeters(double inches) {
+double inches2cm = 2.54;
+return inches * inches2cm;
+}
+
+//Main method
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+//reading user input
+System.out.print("Enter yards ");
+double yards = input.nextDouble();
+System.out.print("Enter feet ");
+double feet = input.nextDouble();
+
+System.out.print("Enter meters ");
+double meters = input.nextDouble();
+System.out.print("Enter inches ");
+double inches = input.nextDouble();
+
+// Displaying the result
+System.out.println("Yards to Feet: " + convertYardsToFeet(yards));
+System.out.println("Feet to Yards: " + convertFeetToYards(feet));
+System.out.println("Inches to Meters: " + convertInchesToMeters(inches));
+System.out.println("Inches to Centimeters: " + convertInchesToCentimeters(inches));
+
+input.close();
+}
+}
