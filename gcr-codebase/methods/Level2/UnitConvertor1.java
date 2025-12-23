e793b74d7fd9cf5f6162d13438d754b7eb38815f
unknown <shwetakumari3405@gmail.com>
2025-12-23 19:52:46 +0530

    [shweta]: added Unit Convertor 1 program

---
+++ gcr-codebase/methods/Level2/UnitConvertor1.java
@@ -0,0 +1,37 @@
+//creating a class with name as UnitConvertor1
+public class UnitConvertor1 {
+// Convert kilometers to miles
+public static double convertKmToMiles(double km) {
+double km2miles = 0.621371;
+return km * km2miles;
+}
+
+// Convert miles to kilometers
+public static double convertMilesToKm(double miles) {
+double miles2km = 1.60934;
+ return miles * miles2km;
+}
+
+// Convert meters to feet
+public static double convertMetersToFeet(double meters) {
+double meters2feet = 3.28084;
+return meters * meters2feet;
+}
+
+// Convert feet to meters
+public static double convertFeetToMeters(double feet) {
+double feet2meters = 0.3048;
+return feet * feet2meters;
+}
+
+public static void main(String[] args) {
+
+//calling the respective methods and printing the results
+System.out.println("10 km = " + convertKmToMiles(10) + " miles");
+System.out.println("5 miles = " + convertMilesToKm(5) + " km");
+System.out.println("3 meters = " + convertMetersToFeet(3) + " feet");
+System.out.println("10 feet = " + convertFeetToMeters(10) + " meters");
+}
+}
+
+
