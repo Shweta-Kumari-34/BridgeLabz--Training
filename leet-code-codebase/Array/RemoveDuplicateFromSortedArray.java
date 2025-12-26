be7af1fd720938805fb38f97ecd30935d51be6f1
unknown <shwetakumari3405@gmail.com>
2025-12-27 01:16:28 +0530

    [shweta]:Remove Duplicate From Sorted Array

---
+++ leet-code-codebase/Array/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,15 @@
+public class RemoveDuplicateFromSortedArray {
+ 
+    public int removeDuplicates(int[] nums) {
+        int count = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[count - 1] != nums[i]){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
