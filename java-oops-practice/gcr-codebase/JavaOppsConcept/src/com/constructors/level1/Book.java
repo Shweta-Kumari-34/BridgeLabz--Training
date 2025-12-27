beb7e761ef3b1fe50b49c4d265c5bed3a552582a
unknown <shwetakumari3405@gmail.com>
2025-12-27 16:30:38 +0530

    [shweta]: Book Program Added

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/constructors/level1/Book.java
@@ -0,0 +1,40 @@
+package com.constructors.level1;
+
+public class Book {
+
+	    String title;
+	    String author;
+	    double price;
+
+	    // Default constructor
+	    Book() {
+	        title = "Unknown";
+	        author = "Unknown";
+	        price = 0.0;
+	    }
+
+	    // Parameterized constructor
+	    Book(String title, String author, double price) {
+	        this.title = title;
+	        this.author = author;
+	        this.price = price;
+	    }
+
+	    void display() {
+	        System.out.println("Title: " + title);
+	        System.out.println("Author: " + author);
+	        System.out.println("Price: " + price);
+	    }
+
+	    public static void main(String[] args) {
+	        Book b1 = new Book();  
+	        b1.display();
+
+	        System.out.println();
+
+	        Book b2 = new Book("Verity", "Colleen Hoover", 499.50);
+	        b2.display();
+	    }
+	}
+
+
