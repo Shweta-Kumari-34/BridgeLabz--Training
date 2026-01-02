e3dc1cfe32319cf5609f65f5bca2c18e9b8fbc31
unknown <shwetakumari3405@gmail.com>
2026-01-02 17:21:19 +0530

    [shweta]:added Main class of fooddeliverysystem

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/encapsulationinterface/fooddeliverysystem/Main.java
@@ -0,0 +1,15 @@
+package com.encapsulationinterface.fooddeliverysystem;
+
+
+	public class Main {
+	    public static void main(String[] args) {
+
+	        FoodItem veg = new VegItem("Paneer Butter Masala", 250, 2);
+	        FoodItem nonVeg = new NonVegItem("Chicken Biryani", 300, 1);
+
+	        OrderProcessor.processOrder(veg);
+	        OrderProcessor.processOrder(nonVeg);
+	    }
+	}
+
+
