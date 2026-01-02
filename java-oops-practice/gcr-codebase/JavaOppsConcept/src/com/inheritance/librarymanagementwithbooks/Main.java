78dec2c696a408e310240c47767d64ed19d22f4a
unknown <shwetakumari3405@gmail.com>
2026-01-02 16:14:34 +0530

    [shweta]:added Main of librarymanagementwithbooks

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/librarymanagementwithbooks/Main.java
@@ -0,0 +1,9 @@
+package com.inheritance.librarymanagementwithbooks;
+
+public class Main {
+	public static void main(String[] args) {
+        Author a1 = new Author("The Alchemist", 1988, "Paulo Coelho", "Brazilian lyricist and novelist");
+        
+        a1.displayInfo();   
+    }
+}
