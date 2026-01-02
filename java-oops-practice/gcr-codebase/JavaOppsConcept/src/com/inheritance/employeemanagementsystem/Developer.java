b57e84af8c37f7fd76f47eae82a4b9e23a26843c
unknown <shwetakumari3405@gmail.com>
2026-01-02 16:06:29 +0530

    [shweta]:added Developer file of employeemanagementsystem

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/employeemanagementsystem/Developer.java
@@ -0,0 +1,16 @@
+package com.inheritance.employeemanagementsystem;
+
+class Developer extends Employee {
+    String programmingLanguage;
+    
+    Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Language: " + programmingLanguage);
+        System.out.println("----------------------------------");
+    }
+}
