4626bb5c9dfb6a6c0fab7b2112a83d87c37aa680
unknown <shwetakumari3405@gmail.com>
2026-01-02 22:23:05 +0530

    [shweta]: added Main class of ridehailingapplication

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/encapsulationinterface/ridehailingapplication/Main.java
@@ -0,0 +1,30 @@
+package com.encapsulationinterface.ridehailingapplication;
+
+public class Main {
+
+
+	    // Polymorphism: same method works for different vehicle types
+	    public static void calculateRideFare(Vehicle vehicle, double distance) {
+	        System.out.println("\n--- Ride Details ---");
+	        vehicle.getVehicleDetails();
+	        System.out.println("Distance: " + distance + " km");
+	        System.out.println("Total Fare: ₹" + vehicle.calculateFare(distance));
+	    }
+
+	    public static void main(String[] args) {
+
+	        Vehicle car = new Car(101, "Rahul");
+	        Vehicle bike = new Bike(102, "Amit");
+	        Vehicle auto = new Auto(103, "Suresh");
+
+	        car.updateLocation("MG Road");
+	        bike.updateLocation("City Center");
+	        auto.updateLocation("Railway Station");
+
+	        calculateRideFare(car, 10);
+	        calculateRideFare(bike, 5);
+	        calculateRideFare(auto, 7);
+	    }
+	}
+
+
