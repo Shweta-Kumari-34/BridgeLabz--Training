81649a821034956a6f5036d0d39358dd799db16d
unknown <shwetakumari3405@gmail.com>
2026-01-02 16:46:18 +0530

    [shweta]:added Person of restaurnatmanagementsystem

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/restaurantmanagementsystem/Person.java
@@ -0,0 +1,21 @@
+package com.inheritance.restaurantmanagementsystem;
+
+class Person {
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+    }
+}
+
+
+interface Worker {
+    void performDuties(); 
+}
