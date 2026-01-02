577c08d350aa3972bbd1e1d6d434e82d2bdfe950
unknown <shwetakumari3405@gmail.com>
2026-01-02 15:51:18 +0530

    [shweta]:added Bird file of animalhierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/animalhierarchy/Bird.java
@@ -0,0 +1,12 @@
+package com.inheritance.animalhierarchy;
+
+class Bird extends Animal{
+	
+    Bird(String name, int age){
+        super(name, age);
+    }
+    
+    void makeSound(){
+        System.out.println(name + " chirps!");
+    }
+}
