165d897be0be933338fefee766477d15160a9e32
unknown <shwetakumari3405@gmail.com>
2026-01-02 16:02:23 +0530

    [shweta]:added Main file of educationalcoursehierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/educationalcoursehierarchy/Main.java
@@ -0,0 +1,10 @@
+package com.inheritance.educationalcoursehierarchy;
+
+public class Main {
+	public static void main(String[] args) {
+
+        PaidOnlineCourse poc = new PaidOnlineCourse("Java Full Stack",12,"Udemy",true,4999,20);
+
+        poc.showDetails();
+    }
+}
