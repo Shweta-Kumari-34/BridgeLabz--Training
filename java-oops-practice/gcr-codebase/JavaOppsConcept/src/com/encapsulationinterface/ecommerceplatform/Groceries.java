63ab10e6af6a610659edd3f442d5b17f6cb40f0f
unknown <shwetakumari3405@gmail.com>
2026-01-02 17:11:27 +0530

    [shweta]:added Groceries class of ecommerceplatform

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/encapsulationinterface/ecommerceplatform/Groceries.java
@@ -0,0 +1,13 @@
+package com.encapsulationinterface.ecommerceplatform;
+
+	class Groceries extends Product {
+	    public Groceries(String id, String name, double price){
+	        super(id, name, price);
+	    }
+  
+	    public double calculateDiscount(){
+	        return getPrice() * 0.03; 
+	    }
+	}
+
+
