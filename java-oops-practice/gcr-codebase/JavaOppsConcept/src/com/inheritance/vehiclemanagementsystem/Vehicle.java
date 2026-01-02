582930b62cdeefa4171f8a49d36f45ebaeaf38a4
unknown <shwetakumari3405@gmail.com>
2026-01-02 16:58:34 +0530

    [shweta]:added Vehicle of vehiclemanagementsystem

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/inheritance/vehiclemanagementsystem/Vehicle.java
@@ -0,0 +1,21 @@
+package com.inheritance.vehiclemanagementsystem;
+
+public class Vehicle {
+	int maxSpeed;
+    String model;
+
+    Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+
+    void showDetails() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
+
+interface Refuelable {
+ void refuel();
+}
+
