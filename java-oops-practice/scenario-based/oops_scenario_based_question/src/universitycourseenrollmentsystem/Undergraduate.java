a8f620a28d0e1b3573e14976e88554b2a0573b15
unknown <shwetakumari3405@gmail.com>
2026-01-03 14:24:12 +0530

    [shweta]: added Undergraduate class of universitycourseenrollmentsystem program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/universitycourseenrollmentsystem/Undergraduate.java
@@ -0,0 +1,11 @@
+package universitycourseenrollmentsystem;
+
+
+	public class Undergraduate extends Student {
+
+	    public Undergraduate(int id, String name) {
+	        super(id, name, "Undergraduate");
+	    }
+	}
+
+
