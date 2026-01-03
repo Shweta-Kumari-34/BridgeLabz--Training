711387246e61fee25b5d84a9e0d52ce539e8f6f3
unknown <shwetakumari3405@gmail.com>
2026-01-03 14:20:45 +0530

    [shweta]: added Postgraduate class of universitycourseenrollmentsystem program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/universitycourseenrollmentsystem/Postgraduate.java
@@ -0,0 +1,11 @@
+package universitycourseenrollmentsystem;
+
+
+	public class Postgraduate extends Student {
+
+	    public Postgraduate(int id, String name) {
+	        super(id, name, "Postgraduate");
+	    }
+	}
+
+
