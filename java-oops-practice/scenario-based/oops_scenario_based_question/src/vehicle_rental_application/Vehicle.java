c2bb63f1f4d9e6bffae68f9ebb545a055d521b3a
unknown <shwetakumari3405@gmail.com>
2026-01-03 15:49:44 +0530

    [shweta]: added Vehicle class of vehicle_rental_application program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/vehicle_rental_application/Vehicle.java
@@ -0,0 +1,30 @@
+package vehicle_rental_application;
+
+public abstract class Vehicle implements Rentable {
+    protected int vehicleId;
+    protected String brand;
+    protected double baseRate;
+
+    public Vehicle(int vehicleId, String brand, double baseRate) {
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.baseRate = baseRate;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getBaseRate() {
+        return baseRate;
+    }
+
+    public void displayInfo() {
+        System.out.println("Vehicle Brand: " + brand + ", Base Rate: ₹" + baseRate);
+    }
+}
+
