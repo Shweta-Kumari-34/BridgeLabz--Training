46baa15eaaf8665a9cf4cec5d58b1402cbc37cb0
unknown <shwetakumari3405@gmail.com>
2026-01-03 14:11:57 +0530

    [shweta]: added Faculty class of universitycourseenrollmentsystem program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/universitycourseenrollmentsystem/Faculty.java
@@ -0,0 +1,16 @@
+package universitycourseenrollmentsystem;
+
+
+public 	class Faculty {
+	    private String facultyName;
+
+	    public Faculty(String facultyName) {
+	        this.facultyName = facultyName;
+	    }
+
+	    public void displayFaculty() {
+	        System.out.println("Faculty: " + facultyName);
+	    }
+	}
+
+
