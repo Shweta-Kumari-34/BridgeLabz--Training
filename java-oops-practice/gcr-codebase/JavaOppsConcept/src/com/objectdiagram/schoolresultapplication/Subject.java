be6a17b29ebf74c38d3544d93def8250155142ce
unknown <shwetakumari3405@gmail.com>
2026-01-03 15:38:41 +0530

    [shweta]: added Subject class of schoolresultapplication program

---
+++ java-oops-practice/gcr-codebase/JavaOppsConcept/src/com/objectdiagram/schoolresultapplication/Subject.java
@@ -0,0 +1,15 @@
+package com.objectdiagram.schoolresultapplication;
+
+public class Subject {
+    private String subjectName;
+    private int marks;
+
+    public Subject(String subjectName, int marks) {
+        this.subjectName = subjectName;
+        this.marks = marks;
+    }
+
+    public String getSubjectName() { return subjectName; }
+    public int getMarks() { return marks; }
+}
+
