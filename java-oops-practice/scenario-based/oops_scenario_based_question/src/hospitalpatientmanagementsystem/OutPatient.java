ca827a89ef5654c9708085f761ae971af2b0cac0
unknown <shwetakumari3405@gmail.com>
2026-01-03 12:59:40 +0530

    [shweta]: added OutPatient class of hospitalpatientmanagementsystem program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/hospitalpatientmanagementsystem/OutPatient.java
@@ -0,0 +1,17 @@
+package hospitalpatientmanagementsystem;
+
+
+	// OutPatient Class
+	public class OutPatient extends Patient {
+
+	    public OutPatient(int id, String name, int age) {
+	        super(id, name, age);
+	    }
+
+	    @Override
+	    public void displayInfo() {
+	        System.out.println("OutPatient: " + name);
+	    }
+	}
+
+
