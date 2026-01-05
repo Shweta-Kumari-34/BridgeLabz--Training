e9bf4b3a02f8730317ab31c530b84d2762ee3ac8
unknown <shwetakumari3405@gmail.com>
2026-01-05 14:03:33 +0530

    [shweta]:added PetrolPump class

---
+++ dsa-question-practice/gcr-codebase/DsaProgramming/src/com/stacksandqueue/circular_tour_problem/PetrolPump.java
@@ -0,0 +1,14 @@
+package com.stacksandqueue.circular_tour_problem;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class PetrolPump {
+	    int petrol;   
+	    int distance;  
+
+	    public PetrolPump(int petrol, int distance) {
+	        this.petrol = petrol;
+	        this.distance = distance;
+	    }
+	}
+
