9bbcfd29d75fb7f99a59bc3210785b6f16ba36a0
unknown <shwetakumari3405@gmail.com>
2026-01-07 10:05:38 +0530

    [shweta]: added strengthWorkout class

--- java-oops-practice/scenario-based/oops_scenario_based_question/src/personalfitnesstracker/StrengthWorkout.java
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/personalfitnesstracker/StrengthWorkout.java
@@ -1,14 +1,13 @@
 package personalfitnesstracker;
 
-	public class StrengthWorkout extends Workout {
+public class StrengthWorkout extends Workout {
 
-	    public StrengthWorkout(int duration) {
-	        super("Strength", duration);
-	    }
-	    public int calculateCalories() {
-	        caloriesBurned = duration * 5;
-	        return caloriesBurned;
-	    }
+	public StrengthWorkout(int duration) {
+		super("Strength", duration);
 	}
 
-
+	public int calculateCalories() {
+		caloriesBurned = duration * 5;
+		return caloriesBurned;
+	}
+}
