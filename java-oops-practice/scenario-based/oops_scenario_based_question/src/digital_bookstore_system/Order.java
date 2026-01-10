63228e3e47c4e023b7e79761c4a0e6ffd5a9025d
unknown <shwetakumari3405@gmail.com>
2026-01-10 07:33:33 +0530

    [shweta]: added Order file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/digital_bookstore_system/Order.java
@@ -0,0 +1,42 @@
+package digital_bookstore_system;
+
+import java.util.ArrayList;
+
+public class Order {
+
+	private ArrayList<Book> bookList;
+	private double totalAmount;
+	private String orderStatus;
+
+	public Order() {
+		bookList = new ArrayList<>();
+		totalAmount = 0;
+		orderStatus = "CREATED";
+	}
+
+	public void addBook(Book book, int quantity) {
+
+		if (book.updateStock(quantity)) {
+
+			double cost = book.getPrice() * quantity;
+			cost = book.applyDiscount(cost);
+
+			totalAmount += cost;
+			bookList.add(book);
+
+			System.out.println(quantity + " copies of \"" + book.getTitle() + "\" added.");
+		} else {
+			System.out.println("Insufficient stock for " + book.getTitle());
+		}
+	}
+
+	private void updateStatus(String status) {
+		this.orderStatus = status;
+	}
+
+	public void placeOrder() {
+		updateStatus("PLACED");
+		System.out.println("\nOrder Status: " + orderStatus);
+		System.out.println("Total Payable Amount: ₹" + totalAmount);
+	}
+}
