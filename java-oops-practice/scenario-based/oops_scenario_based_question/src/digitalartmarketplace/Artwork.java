2986fbed89ec628d8fb5dde8e1ea743b27ada30d
unknown <shwetakumari3405@gmail.com>
2026-01-10 07:37:29 +0530

    [shweta]: added Artwork file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/digitalartmarketplace/Artwork.java
@@ -0,0 +1,25 @@
+package digitalartmarketplace;
+
+public   class Artwork {
+	
+protected String title;
+protected String artist;
+protected double price;
+protected String licenseType;
+
+
+Artwork(String title, String artist, double price, String licenseType ){
+	this.title = title;
+	this.artist=artist;
+	this.price= price;
+	this.licenseType= licenseType;
+	
+	
+}
+
+
+
+
+
+
+}
