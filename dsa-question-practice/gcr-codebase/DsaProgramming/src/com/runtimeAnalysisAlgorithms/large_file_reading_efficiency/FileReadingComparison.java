35f57122284abcea3d4a1e666ca9143691ca0adc
unknown <shwetakumari3405@gmail.com>
2026-01-11 23:17:07 +0530

    [shweta]: added program

--- dsa-question-practice/gcr-codebase/DsaProgramming/src/com/runtimeAnalysisAlgorithms/large_file_reading_efficiency/FileReadingComparison.java
+++ dsa-question-practice/gcr-codebase/DsaProgramming/src/com/runtimeAnalysisAlgorithms/large_file_reading_efficiency/FileReadingComparison.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class FileReadingComaparison {
+public class FileReadingComparison {
 
 	public static void main(String[] args) throws IOException {
 
