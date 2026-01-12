9e8a25855d6d3370f5ea538e848ab7ab0cfd0fb3
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:10:22 +0530

    [shweta]: added User file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/online_learning_portal/User.java
@@ -0,0 +1,11 @@
+package online_learning_portal;
+
+public abstract class User {
+    protected String name;
+    protected String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
