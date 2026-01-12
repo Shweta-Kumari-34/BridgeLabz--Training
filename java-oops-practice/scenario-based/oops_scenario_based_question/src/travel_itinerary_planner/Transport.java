bb9c1e3c4395872d966be30174f684702992f096
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:34:55 +0530

    [shweta]: added Transport file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/travel_itinerary_planner/Transport.java
@@ -0,0 +1,13 @@
+package travel_itinerary_planner;
+
+public class Transport {
+	 private double cost;
+
+	    public Transport(double cost) {
+	        this.cost = cost;
+	    }
+
+	    double getCost() {   
+	        return cost;
+	    }
+}
