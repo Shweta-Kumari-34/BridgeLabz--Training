5c79582776ded8db65e0eebb874376f26a22a7a9
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:25:14 +0530

    [shweta]: added Activity file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/travel_itinerary_planner/Activity.java
@@ -0,0 +1,13 @@
+package travel_itinerary_planner;
+
+public class Activity {
+	private double cost;
+
+    public Activity(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
