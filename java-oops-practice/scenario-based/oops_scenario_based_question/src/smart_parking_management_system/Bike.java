f3a6994bebe143f9cf3bc605592db5eb710b44e3
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:19:22 +0530

    [shweta]: added Bike file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/smart_parking_management_system/Bike.java
@@ -0,0 +1,14 @@
+package smart_parking_management_system;
+
+public class Bike extends Vehicle {
+
+    public Bike(String vehicleNumber) {
+        super(vehicleNumber, 30); 
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double penalty = hours > 6 ? 50 : 0;
+        return (baseRate * hours) + penalty;
+    }
+}
