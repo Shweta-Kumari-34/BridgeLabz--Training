1e3822ca6efd82349c0e2f520e885783d6a598ed
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:35:36 +0530

    [shweta]: added Trip file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/travel_itinerary_planner/Trip.java
@@ -0,0 +1,30 @@
+package travel_itinerary_planner;
+
+public abstract class Trip implements IBookable {
+
+    protected String destination;
+    protected int duration;
+    protected double totalBudget;
+
+    protected Transport transport;
+    protected Hotel hotel;
+    protected Activity activity;
+
+    public Trip(String destination, int duration,
+                Transport transport, Hotel hotel, Activity activity) {
+
+        this.destination = destination;
+        this.duration = duration;
+        this.transport = transport;
+        this.hotel = hotel;
+        this.activity = activity;
+        
+        this.totalBudget = transport.getCost()+hotel.getCost()+activity.getCost();
+
+    }
+
+    public double getTotalBudget() {
+        return totalBudget;
+    }
+}
+
