5a487408595403ec21dc2632508d3e88542c5854
unknown <shwetakumari3405@gmail.com>
2026-01-12 16:19:55 +0530

    [shweta]: added Car file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/smart_parking_management_system/Car.java
@@ -0,0 +1,15 @@
+package smart_parking_management_system;
+
+public class Car extends Vehicle {
+
+    public Car(String vehicleNumber) {
+        super(vehicleNumber, 50); 
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double penalty = hours > 5 ? 100 : 0;
+        return (baseRate * hours) + penalty;
+    }
+}
+
