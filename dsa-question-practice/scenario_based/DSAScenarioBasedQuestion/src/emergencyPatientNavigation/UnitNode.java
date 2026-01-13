c6972309fd74b5a269efdcf8eaaedc1fb3644afa
unknown <shwetakumari3405@gmail.com>
2026-01-13 13:04:54 +0530

    [shweta]: added UnitNode file

---
+++ dsa-question-practice/scenario_based/DSAScenarioBasedQuestion/src/emergencyPatientNavigation/UnitNode.java
@@ -0,0 +1,13 @@
+package emergencyPatientNavigation;
+
+public class UnitNode {
+	String unitName;
+    boolean available;
+    UnitNode next;
+
+    UnitNode(String unitName, boolean available) {
+        this.unitName = unitName;
+        this.available = available;
+        this.next = null;
+    }
+}
