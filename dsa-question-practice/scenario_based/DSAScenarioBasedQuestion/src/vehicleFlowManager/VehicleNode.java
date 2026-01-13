e99f31b7525ad1b1d484e9149450faa30490c109
unknown <shwetakumari3405@gmail.com>
2026-01-13 13:15:07 +0530

    [shweta]: added VehicleNode file

---
+++ dsa-question-practice/scenario_based/DSAScenarioBasedQuestion/src/vehicleFlowManager/VehicleNode.java
@@ -0,0 +1,10 @@
+package vehicleFlowManager;
+
+public class VehicleNode {
+	String vehicleNumber;
+    VehicleNode next;
+
+    VehicleNode(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+}
