55052c2a1f66aa0d97b300d0e61376190ffe9952
unknown <shwetakumari3405@gmail.com>
2026-01-13 13:07:19 +0530

    [shweta]: added Book file

---
+++ dsa-question-practice/scenario_based/DSAScenarioBasedQuestion/src/library_organizer/Book.java
@@ -0,0 +1,19 @@
+package library_organizer;
+
+class Book {
+    String title;
+    String author;
+    String isbn;   
+
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author;
+    }
+}
+
