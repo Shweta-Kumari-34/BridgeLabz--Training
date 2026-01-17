e415ebedc67d11f127c77b559a09654dc44d2ffb
unknown <shwetakumari3405@gmail.com>
2026-01-17 07:23:03 +0530

    [shweta]: added Main file

---
+++ dsa-question-practice/scenario_based/DSAScenarioBasedQuestion/src/deliveryChainManagement/Main.java
@@ -0,0 +1,16 @@
+package deliveryChainManagement;
+
+public class Main {
+	public static void main(String[] args) {
+
+        ParcelTracker tracker = new ParcelTracker();
+
+        tracker.trackParcel();
+
+        tracker.addCheckpoint("Shipped", "Customs Check");
+        tracker.trackParcel();
+
+        tracker.markParcelLost();
+        tracker.trackParcel();
+    }
+}
