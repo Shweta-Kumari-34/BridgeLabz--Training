f7db4979a0b187d25d1ce16ce61698101166954a
unknown <shwetakumari3405@gmail.com>
2026-01-19 21:01:55 +0530

    [shweta]:added DeveloperInfo file

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/birdsanctuaryapp/DeveloperInfo.java
@@ -0,0 +1,11 @@
+package birdsanctuaryapp;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DeveloperInfo {
+    String developer();
+    String version();
+}
+
