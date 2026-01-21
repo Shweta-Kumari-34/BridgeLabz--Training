af541427ad5d0d844de8450e1a25bbd9cdcac7f5
unknown <shwetakumari3405@gmail.com>
2026-01-21 08:15:41 +0530

    [shweta]:added Program File of Smart Warehouse Management

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/generics/warehousemanagementsystem/WarehouseItem.java
@@ -0,0 +1,21 @@
+package generics.warehousemanagementsystem;
+
+public abstract class WarehouseItem {
+	private String name;
+	private double price;
+
+	public WarehouseItem(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public abstract void displayInfo();
+}
