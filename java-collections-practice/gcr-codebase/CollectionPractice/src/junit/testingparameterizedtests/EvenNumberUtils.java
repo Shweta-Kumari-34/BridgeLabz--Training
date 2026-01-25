e1ec5722173042279e6a4f363fa04b2cc3504d7d
unknown <shwetakumari3405@gmail.com>
2026-01-25 22:42:38 +0530

    [shweta]: added Even Number Utils program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/junit/testingparameterizedtests/EvenNumberUtils.java
@@ -0,0 +1,8 @@
+package junit.testingparameterizedtests;
+
+public class EvenNumberUtils {
+	// Method to check if a number is even
+	public boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+}
