b80c0c5a2714e60bb09288652a3b38bfab6b11f5
unknown <shwetakumari3405@gmail.com>
2026-01-26 23:08:23 +0530

    [shweta]: added file of University's Digital Record System program

---
+++ dsa-question-practice/scenario_based/DSAScenarioBasedQuestion/src/treedatastructure/binarysearchtree/Node.java
@@ -0,0 +1,12 @@
+package treedatastructure.binarysearchtree;
+
+public class Node {
+	int key;
+	String data;
+	Node left, right;
+
+	public Node(int key, String data) {
+		this.key = key;
+		this.data = data;
+	}
+}
