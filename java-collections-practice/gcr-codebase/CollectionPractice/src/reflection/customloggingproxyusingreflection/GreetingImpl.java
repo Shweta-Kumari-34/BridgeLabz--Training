af70d583a2607fbb68eac25847888f3b03fa1ca4
unknown <shwetakumari3405@gmail.com>
2026-01-26 22:08:11 +0530

    [shweta]: added file of Custom Logging Proxy Using Reflection program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/reflection/customloggingproxyusingreflection/GreetingImpl.java
@@ -0,0 +1,8 @@
+package reflection.customloggingproxyusingreflection;
+
+public class GreetingImpl implements Greeting {
+	// Actual method implementation
+    public void sayHello() {
+        System.out.println("Hello! Welcome to Reflection");
+    }
+}
