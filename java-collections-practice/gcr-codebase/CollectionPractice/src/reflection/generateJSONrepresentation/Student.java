6c5ebf20e236cfea599ebc13c61e42c472c0b5d4
unknown <shwetakumari3405@gmail.com>
2026-01-26 22:14:55 +0530

    [shweta]: added file of Generate a JSON Representation program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/reflection/generateJSONrepresentation/Student.java
@@ -0,0 +1,13 @@
+package reflection.generateJSONrepresentation;
+// POJO class
+public class Student {
+	 // Fields
+    private String name;
+    private int age;
+
+    // Constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
