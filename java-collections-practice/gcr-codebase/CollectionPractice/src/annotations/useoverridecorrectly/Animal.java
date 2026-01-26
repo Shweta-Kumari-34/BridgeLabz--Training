044d1fca38d84d0645cf2bea6761fec225aa3246
unknown <shwetakumari3405@gmail.com>
2026-01-26 21:02:00 +0530

    [shweta]: added file of Use @Override Correctly program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/annotations/useoverridecorrectly/Animal.java
@@ -0,0 +1,8 @@
+package annotations.useoverridecorrectly;
+
+public class Animal {
+	 // method to be overridden
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
