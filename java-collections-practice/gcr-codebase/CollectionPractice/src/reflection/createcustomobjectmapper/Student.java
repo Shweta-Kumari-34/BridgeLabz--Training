d32db5cc192bc32f72594b74bda2422605574ba0
unknown <shwetakumari3405@gmail.com>
2026-01-26 22:04:58 +0530

    [shweta]: added file of Create a Custom Object Mapper program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/reflection/createcustomobjectmapper/Student.java
@@ -0,0 +1,13 @@
+package reflection.createcustomobjectmapper;
+
+public class Student {
+	 // Fields
+    private String name;
+    private int age;
+
+    // Method to display data
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
