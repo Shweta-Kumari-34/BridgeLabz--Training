24c0ffebb57954b077ebe7f139943b11a6033e52
unknown <shwetakumari3405@gmail.com>
2026-01-26 22:16:35 +0530

    [shweta]: added file of Dynamically Create Objects program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/reflection/dynamicallycreateobjects/Student.java
@@ -0,0 +1,8 @@
+package reflection.dynamicallycreateobjects;
+
+public class Student {
+	// Method to show message
+	public void display() {
+		System.out.println("Student object created using Reflection");
+	}
+}
