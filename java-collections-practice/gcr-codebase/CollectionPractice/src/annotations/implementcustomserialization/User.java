cf8d135ad4d5cf92d7f56df8e10cd26c04641862
unknown <shwetakumari3405@gmail.com>
2026-01-26 21:33:17 +0530

    [shweta]: added file of Implement a Custom Serialization program

---
+++ java-collections-practice/gcr-codebase/CollectionPractice/src/annotations/implementcustomserialization/User.java
@@ -0,0 +1,17 @@
+package annotations.implementcustomserialization;
+
+public class User {
+
+    @JsonField(name = "user_name")
+    private String username;
+
+    @JsonField(name = "user_age")
+    private int age;
+
+    // constructor
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
+
