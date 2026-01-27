2830bdb63bf8c8b6c6c7d2616b461a4e686f43f9
unknown <shwetakumari3405@gmail.com>
2026-01-27 22:20:33 +0530

    [shweta]: added file of Java Object to JSON array program

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/main/java/com/jsondata/convertjava/Student.java
@@ -0,0 +1,25 @@
+package com.jsondata.convertjava;
+
+public class Student {
+
+	// student name
+	private String name;
+
+	// student age
+	private int age;
+
+	// constructor
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// getters
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
