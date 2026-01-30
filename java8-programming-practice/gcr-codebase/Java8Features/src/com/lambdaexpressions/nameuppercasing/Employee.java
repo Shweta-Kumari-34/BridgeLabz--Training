900fc25d9e097733f826344f82bac14e5b3f9000
unknown <shwetakumari3405@gmail.com>
2026-01-30 17:08:55 +0530

    [shweta]: added file of Convert Employee Name Uppercase For HR Letter program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/lambdaexpressions/nameuppercasing/Employee.java
@@ -0,0 +1,16 @@
+package com.lambdaexpressions.nameuppercasing;
+
+public class Employee {
+
+	private String name;
+
+//setters
+	public Employee(String name) {
+		this.name = name;
+	}
+
+	// getter
+	public String getName() {
+		return name;
+	}
+}
