30ac426259f13ebdd5fbc77297652214fcef046e
unknown <shwetakumari3405@gmail.com>
2026-02-01 17:48:09 +0530

    [shweta]: added file of Smart Device Control program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/functionalinterfaces/smartdevicecontrol/Light.java
@@ -0,0 +1,15 @@
+package com.functionalinterfaces.smartdevicecontrol;
+
+//Light implements SmartDevice interface
+public class Light implements SmartDevice {
+
+	@Override
+	public void turnOn() {
+		System.out.println("Light is turned ON");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("Light is turned OFF");
+	}
+}
