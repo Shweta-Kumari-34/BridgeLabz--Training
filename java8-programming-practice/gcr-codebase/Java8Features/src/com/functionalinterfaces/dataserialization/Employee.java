9c859f76a7fee16efa323ff3d9928228441c3b41
unknown <shwetakumari3405@gmail.com>
2026-02-01 17:30:57 +0530

    [shweta]: added file of Data Serialization for Backup program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/functionalinterfaces/dataserialization/Employee.java
@@ -0,0 +1,17 @@
+package com.functionalinterfaces.dataserialization;
+
+public class Employee implements Backupable {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', id=" + id + "}";
+    }
+}
+
