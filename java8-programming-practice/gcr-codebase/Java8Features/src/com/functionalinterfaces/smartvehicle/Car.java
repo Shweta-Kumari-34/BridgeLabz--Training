02d68a80d90cfd431d4244649303f27525daeae1
unknown <shwetakumari3405@gmail.com>
2026-02-01 17:50:30 +0530

    [shweta]: added file of Smart Vehicle Dashboard program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/functionalinterfaces/smartvehicle/Car.java
@@ -0,0 +1,9 @@
+package com.functionalinterfaces.smartvehicle;
+
+public class Car implements Vehicle {
+
+	@Override
+	public void displaySpeed() {
+		System.out.println("Car speed: 80 km/h");
+	}
+}
