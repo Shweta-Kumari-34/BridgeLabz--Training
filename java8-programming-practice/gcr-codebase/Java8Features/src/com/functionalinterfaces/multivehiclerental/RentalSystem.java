ec5b1ff3e80e95166718c48263a478bb2da4f15f
unknown <shwetakumari3405@gmail.com>
2026-02-01 17:37:47 +0530

    [shweta]: added file of Multi-Vehicle Rental System program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/functionalinterfaces/multivehiclerental/RentalSystem.java
@@ -0,0 +1,19 @@
+package com.functionalinterfaces.multivehiclerental;
+
+public class RentalSystem {
+
+	public static void main(String[] args) {
+
+		Vehicle car = new Car();
+		Vehicle bike = new Bike();
+		Vehicle bus = new Bus();
+
+		car.rent();
+		bike.rent();
+		bus.rent();
+
+		car.returnVehicle();
+		bike.returnVehicle();
+		bus.returnVehicle();
+	}
+}
