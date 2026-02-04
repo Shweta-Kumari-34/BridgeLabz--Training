e643e6a632612bac928f40efe64aead51df6e053
unknown <shwetakumari3405@gmail.com>
2026-02-04 18:56:59 +0530

    [shweta]: added file of smartcity program

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/workshopprogram/smartcity/AmbulanceService.java
@@ -0,0 +1,8 @@
+package com.workshop.smartcity;
+
+public class AmbulanceService implements TransportService, EmergencyService {
+
+	public String getServiceName() {
+		return "Ambulance Service";
+	}
+}
