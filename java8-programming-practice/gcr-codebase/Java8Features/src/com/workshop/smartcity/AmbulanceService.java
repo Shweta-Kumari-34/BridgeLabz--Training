6e3163b277e6dd5f20e3ebb762061ad83c72ce1f
unknown <shwetakumari3405@gmail.com>
2026-02-04 18:47:40 +0530

    [shweta]: delete wrong file

--- java8-programming-practice/gcr-codebase/Java8Features/src/com/workshop/smartcity/AmbulanceService.java
+++
@@ -1,8 +0,0 @@
-package com.workshop.smartcity;
-
-public class AmbulanceService implements TransportService, EmergencyService {
-
-	public String getServiceName() {
-		return "Ambulance Service";
-	}
-}
