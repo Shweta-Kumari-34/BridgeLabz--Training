6ef4c89bfc103ec2911328fa3b876d48ce420169
unknown <shwetakumari3405@gmail.com>
2026-02-04 18:49:56 +0530

    [shweta]: delete wrong file

--- java8-programming-practice/gcr-codebase/Java8Features/src/com/workshop/smartcity/GeoUtils.java
+++
@@ -1,9 +0,0 @@
-package com.workshop.smartcity;
-
-public interface GeoUtils {
-
-	// static utility method
-	static double calculateDistance(double start, double end) {
-		return Math.abs(end - start); // simple distance logic
-	}
-}
