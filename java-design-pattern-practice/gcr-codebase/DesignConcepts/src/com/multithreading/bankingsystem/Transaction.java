b2d1002876c3eb3312719621c396d6957fb8405b
unknown <shwetakumari3405@gmail.com>
2026-02-05 23:09:58 +0530

    [shweta]: added file of Banking System with Multiple Transactions program

---
+++ java-design-pattern-practice/gcr-codebase/DesignConcepts/src/com/multithreading/bankingsystem/Transaction.java
@@ -0,0 +1,42 @@
+package com.multithreading.bankingsystem;
+
+import java.time.LocalDateTime;
+
+class Transaction implements Runnable {
+
+    private BankAccount account;
+    private String customerName;
+    private int amount;
+
+    public Transaction(BankAccount account,
+                       String customerName,
+                       int amount) {
+        this.account = account;
+        this.customerName = customerName;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("[" + Thread.currentThread().getName() +
+                "] Attempting to withdraw " + amount);
+
+        boolean success = account.withdraw(amount);
+
+        if (success) {
+            System.out.println("Transaction successful: " +
+                    customerName +
+                    ", Amount: " + amount +
+                    ", Balance: " + account.getBalance() +
+                    ", Time: " + LocalDateTime.now());
+        } else {
+            System.out.println("Transaction FAILED: " +
+                    customerName +
+                    ", Amount: " + amount +
+                    ", Balance: " + account.getBalance() +
+                    ", Time: " + LocalDateTime.now());
+        }
+    }
+}
+
