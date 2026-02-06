1b696cacd36b525c32502696e9009dffb353f8ad
unknown <shwetakumari3405@gmail.com>
2026-02-06 21:51:10 +0530

    [shweta]: added file of Auto Audit System program

---
+++ java-iostreams-practice/scenario-based/ScenarioBased/src/main/java/iostreamscenariobased/ScenarioBased/eventtrackerapp/UserActions.java
@@ -0,0 +1,16 @@
+package iostreamscenariobased.ScenarioBased.eventtrackerapp;
+
+public class UserActions {
+
+    @AuditTrail(action = "USER_LOGIN", severity = "LOW")
+    public void login() {}
+
+    @AuditTrail(action = "FILE_UPLOAD", severity = "MEDIUM")
+    public void uploadFile() {}
+
+    @AuditTrail(action = "FILE_DELETE", severity = "HIGH")
+    public void deleteFile() {}
+
+    public void viewProfile() {} 
+}
+
