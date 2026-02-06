1a088bf100631fcb6819ee406f5c1e35be5f5110
unknown <shwetakumari3405@gmail.com>
2026-02-06 22:01:26 +0530

    [shweta]: added file of Student Marksheet Generator  program

---
+++ java-iostreams-practice/scenario-based/ScenarioBased/src/main/java/iostreamscenariobased/ScenarioBased/markmateapp/Student.java
@@ -0,0 +1,19 @@
+package iostreamscenariobased.ScenarioBased.markmateapp;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private int total;
+    private double average;
+    private String grade;
+
+    public Student(int studentId, String name, int total, double average, String grade) {
+        this.studentId = studentId;
+        this.name = name;
+        this.total = total;
+        this.average = average;
+        this.grade = grade;
+    }
+}
+
