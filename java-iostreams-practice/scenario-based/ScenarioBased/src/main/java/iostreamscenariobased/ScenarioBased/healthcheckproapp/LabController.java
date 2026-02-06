f783aa95cf99027cdf597ccf63088fd066fc30f1
unknown <shwetakumari3405@gmail.com>
2026-02-06 21:54:45 +0530

    [shweta]: added file of HealthCheckPro (API Metadata Validator) program

---
+++ java-iostreams-practice/scenario-based/ScenarioBased/src/main/java/iostreamscenariobased/ScenarioBased/healthcheckproapp/LabController.java
@@ -0,0 +1,17 @@
+package iostreamscenariobased.ScenarioBased.healthcheckproapp;
+
+
+
+public class LabController {
+
+	@PublicAPI
+	public void viewTests() {
+	}
+
+	@RequiresAuth
+	public void bookTest() {
+	}
+
+	public void deleteTest() {
+	}
+}
