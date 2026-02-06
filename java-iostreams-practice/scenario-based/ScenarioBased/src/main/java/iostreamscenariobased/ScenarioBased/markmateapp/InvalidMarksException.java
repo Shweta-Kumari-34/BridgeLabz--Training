8277b7316e87ec370fee43ca4aba1b036513741d
unknown <shwetakumari3405@gmail.com>
2026-02-06 21:59:29 +0530

    [shweta]: added file of Student Marksheet Generator  program

---
+++ java-iostreams-practice/scenario-based/ScenarioBased/src/main/java/iostreamscenariobased/ScenarioBased/markmateapp/InvalidMarksException.java
@@ -0,0 +1,8 @@
+package iostreamscenariobased.ScenarioBased.markmateapp;
+
+public class InvalidMarksException extends Exception {
+    public InvalidMarksException(String message) {
+        super(message);
+    }
+}
+
